592714b977b39ab6dfee85fab7554c1ab505036b
Gaddar Kr Chaudhary <gaddarkumar7447@gmail.com>
2022-09-10 17:01:58 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.buttonPanel)
+        button.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this, Next :: class.java)
+            startActivity(intent)
+        })
+
+    }
+}
