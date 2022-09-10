3c9d7e00597c5af9ec643a65f69ea847cb8df705
Gaddar Kr Chaudhary <gaddarkumar7447@gmail.com>
2022-09-10 17:52:27 +0530

    some changes

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +16,9 @@ class MainActivity : AppCompatActivity() {
         button.setOnClickListener(View.OnClickListener {
             val intent = Intent(this, Next :: class.java)
             startActivity(intent)
+            val myToast = Toast.makeText(applicationContext,"toast message with gravity",Toast.LENGTH_SHORT)
+            myToast.setGravity(Gravity.LEFT,400,400)
+            myToast.show()
         })
 
     }
--- app/src/main/java/com/example/myapplication/Next.kt
+++ app/src/main/java/com/example/myapplication/Next.kt
@@ -1,12 +1,31 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class Next : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_next)
+        val editText = findViewById<EditText>(R.id.input_et)
+        val button = findViewById<TextView>(R.id.generate_bn)
+        val show = findViewById<TextView>(R.id.showText)
+
+
+        button.setOnClickListener(View.OnClickListener {
+            val string : String = editText.text.toString().trim()
+            if (string.isEmpty()){
+                editText.error = "Write something"
+            }else{
+                show.text = string
+            }
+        })
 
     }
 }
