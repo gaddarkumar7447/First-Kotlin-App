c2a1bf17fe29eebf4fcbfdcaa6f233f207ea2967
Gaddar Kr Chaudhary <gaddarkumar7447@gmail.com>
2022-09-17 21:47:26 +0530

    Model View implemented

--- app/src/main/java/com/example/myapplication/BindingClass.kt
+++ app/src/main/java/com/example/myapplication/BindingClass.kt
@@ -2,11 +2,44 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingComponent
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.example.myapplication.databinding.ActivityBindingClassBinding
 
 class BindingClass : AppCompatActivity() {
+    private lateinit var bindingClass : ActivityBindingClassBinding
+
+    private lateinit var viewModel : BindingModelClass
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_binding_class)
+        bindingClass = DataBindingUtil.setContentView(this, R.layout.activity_binding_class)
 
+        viewModel = ViewModelProvider(this).get(BindingModelClass::class.java)
+
+        bindingClass.showText.text = viewModel.getCurrentCount().toString()
+
+        bindingClass.increment.setOnClickListener(View.OnClickListener {
+
+            bindingClass.showText.text = viewModel.updateCount().toString()
+
+        })
+
+        /*bindingClass.buttonSubmit.setOnClickListener(View.OnClickListener {
+            showData()
+        })
+        var a = 0;
+        bindingClass.increment.setOnClickListener(View.OnClickListener {
+            a++
+            bindingClass.showText.text = a.toString()
+        })*/
     }
+
+    /*private fun showData(){
+        bindingClass.textView.text = bindingClass.editText.text.toString().trim()
+    }*/
+
+
 }
---
+++ app/src/main/java/com/example/myapplication/BindingModelClass.kt
@@ -0,0 +1,16 @@
+package com.example.myapplication
+
+import androidx.lifecycle.ViewModel
+
+class BindingModelClass : ViewModel() {
+    var count = 0
+
+    fun getCurrentCount() : Int{
+        return count
+    }
+
+    fun updateCount() : Int{
+        return count++
+    }
+
+}
